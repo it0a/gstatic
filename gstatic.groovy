9f25828a9f61be11655bed238b325ec774c189b7
it0a <recursive404@gmail.com>
2015-03-10 01:13:04 -0400

    finishing data generation

--- gstatic.groovy
+++ gstatic.groovy
@@ -1,12 +1,8 @@
 @Grab('mysql:mysql-connector-java:5.1.25')
-@Grab('log4j:log4j:1.2.17')
 @GrabConfig(systemClassLoader = true)
 import groovy.transform.CompileStatic
 import groovy.sql.Sql
-import org.apache.log4j.*
-import groovy.util.logging.*
 
-@Log4j
 class GStatic {
 
     def perfExec = { name, c ->
@@ -32,16 +28,75 @@ class GStatic {
         ].each{sql.execute(it)}}
     }
 
-    def generateData() {
-        sql.execute("""
+    @CompileStatic
+    List<Date> generateDates(int n) {
+        (1..n).collect{new Date() + n}
+    }
+
+    @CompileStatic
+    void generateContractData(Sql sql, Closure c) {
+        sql.withBatch("""
             INSERT INTO contract (date_start)
             VALUES (?)
-        """, [new Date()])
+        """, c)
+    }
+
+    @CompileStatic
+    String buildInvoiceQuery(Sql sql) {
+        (0..((12*3)-1))
+        .collect{ "UNION ALL SELECT id AS id, DATE_ADD(date_start, INTERVAL ${it} MONTH) AS date_invoiced FROM contract"}
+        .join(" ")
+        .minus("UNION ALL ")
+    }
+
+    @CompileStatic
+    String buildLineItemQuery() {
+        ((1..19)
+        .collect{"UNION ALL SELECT id AS id, ROUND(RAND() * 1000, 2) AS amount FROM invoice"}
+        .join(' ')
+        .minus("UNION ALL"))
+    }
+
+
+    @CompileStatic
+    void generateInvoiceData(Sql sql, Closure c) {
+        sql.withBatch("""
+            INSERT INTO invoice (contract_id, date_invoiced, number)
+            VALUES (?, ?, ?)
+        """, c)
+    }
+
+    @CompileStatic
+    void generateLineItemData(Sql sql, Closure c) {
+        sql.withBatch("""
+            INSERT INTO line_item (invoice_id, amount)
+            VALUES (?, ?)
+        """, c)
+    }
+
+    def generateData(data) {
+        sql.withTransaction {
+            perfExec("generateContractData", {->
+                generateContractData(sql, { stmt -> generateDates(30).each{ d -> stmt.addBatch([d])}})
+            })
+            perfExec("generateInvoiceData", {->
+                generateInvoiceData(sql, { stmt ->
+                    sql.rows(buildInvoiceQuery()).eachWithIndex{invoice, i -> stmt.addBatch([invoice.id, invoice.date_invoiced, i + 1])}
+                })
+            })
+            perfExec("generateLineItemData", {->
+                generateLineItemData(sql, { stmt -> sql.rows(buildLineItemQuery()).each{stmt.addBatch([it.id, it.amount])} })
+            })
+        }
     }
 
     def run() {
-        perfExec("clearTables", {-> clearTables()})
-        perfExec("generateData", {-> generateData()})
+        perfExec("clearTables", {->
+            clearTables()
+        })
+        perfExec("generateData", {->
+            generateData()
+        })
     }
 
 }
