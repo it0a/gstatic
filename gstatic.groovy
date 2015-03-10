c63c72f1d4614762b19662b6c96e5e6fc4a3f04c
it0a <recursive404@gmail.com>
2015-03-09 23:37:20 -0400

    Hooking up timing functionality

--- gstatic.groovy
+++ gstatic.groovy
@@ -1,35 +1,47 @@
+@Grab('mysql:mysql-connector-java:5.1.25')
+@Grab('log4j:log4j:1.2.17')
+@GrabConfig(systemClassLoader = true)
 import groovy.transform.CompileStatic
+import groovy.sql.Sql
+import org.apache.log4j.*
+import groovy.util.logging.*
 
+@Log4j
 class GStatic {
+
+    def perfExec = { name, c ->
+        def startTime = System.currentTimeMillis()
+        c()
+        def endTime = System.currentTimeMillis() - startTime
+        println "${name} finished in ${endTime}ms"
+    }
+
+    Sql sql = Sql.newInstance("jdbc:mysql://localhost:3306/test_data", "root", "sriq@", "com.mysql.jdbc.Driver")
 
     static main(args) {
         new GStatic().run()
     }
 
-    @CompileStatic
-    def loadData() {
-        [
-            [
-                id: 1,
-                children: [
-                    [id: 3, type: "B"],
-                    [id: 4, type: "B"],
-                    [id: 5, type: "B"]
-                ]
-            ],
-            [
-                id: 2,
-                children: [
-                    [id: 6, type: "B"],
-                    [id: 7, type: "B"],
-                    [id: 8, type: "B"]
-                ]
-            ],
-        ]
+    void clearTables() {
+        sql.withTransaction {[
+            "SET FOREIGN_KEY_CHECKS = 0",
+            "TRUNCATE TABLE contract",
+            "TRUNCATE TABLE invoice",
+            "TRUNCATE TABLE line_item",
+            "SET FOREIGN_KEY_CHECKS = 1"
+        ].each{sql.execute(it)}}
+    }
+
+    def generateData() {
+        sql.execute("""
+            INSERT INTO contract (date_start)
+            VALUES (?)
+        """, [new Date()])
     }
 
     def run() {
-        println loadData()
+        perfExec("clearTables", {-> clearTables()})
+        perfExec("generateData", {-> generateData()})
     }
 
 }
